066778c344006932d1cc61ce86e63d329ee73718
Muzammil-2006 <mm7194315@gmail.com>
2026-09-09 11:24:05 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter a num: "))
-b=int(input("Enter a num: "))
-c=int(input("Enter a num: "))
-if a==b and b==c:
-    print("All three are equal")
-else:
-    print("All there are not equal")    
